589802118934affecf3e828e43b3f49ea4631c0a
mLoDar <mlodarcoding@gmail.com>
2024-11-11 14:29:26 +0100

    Adjusted user interface The input section received a "facelift" as it looked kind of unfinished. Also reworked the way how the input is read into the application.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Metrics;
+using System.Text;
 
 
 
@@ -10,8 +11,6 @@ namespace Mazescape
     {
         private static readonly ApplicationSettings _appSettings = new();
 
-        private static char[,] _mazeLayout = new char[0, 0];
-
 
 
         static void Main()
@@ -20,19 +19,34 @@ namespace Mazescape
 
             Console.Title = "Mazescape";
             Console.OutputEncoding = Encoding.UTF8;
-            
+
             Console.Clear();
             Console.SetCursorPosition(0, 4);
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+
+            Console.WriteLine("           ┳┳┓                ");
+            Console.WriteLine("           ┃┃┃┏┓┓┏┓┏┏┏┓┏┓┏┓   ");
+            Console.WriteLine("           ┛ ┗┗┻┗┗ ┛┗┗┻┣┛┗    ");
+            Console.Write("                       ┛     ");
+
             Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("│");
+            Console.WriteLine("        ┌────────────────────┘");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
 
 
 
-            int mazeHeight = ReadInputAsInteger("Enter a height for the maze:", _appSettings.maxMazeLayoutHeight);
-            int mazeWidth = ReadInputAsInteger("Enter a width for the maze:", _appSettings.maxMazeLayoutWidth);
+            int mazeHeight = ReadInputAsInteger("Adjust the height for the maze:", 10, _appSettings.maxMazeLayoutHeight);
+            int mazeWidth = ReadInputAsInteger("Adjust the width for the maze:", 30, _appSettings.maxMazeLayoutWidth);
 
             int maxCountOfObstacles = mazeHeight * mazeWidth * _appSettings.maxPercentOfObstaclesInLayout / 100;
-            int obstacleCount = ReadInputAsInteger("Define the amount of obstacles:", maxCountOfObstacles);
-
+            int obstacleCount = ReadInputAsInteger("Define the amount of obstacles:", maxCountOfObstacles / 3, maxCountOfObstacles);
+            
 
 
             MazeLayout mazeLayout = new(mazeHeight, mazeWidth);
@@ -49,39 +63,74 @@ namespace Mazescape
 
             MazeLayout.Display();
 
-            
+
 
             goto LabelMethodBeginning;
         }
 
-        private static int ReadInputAsInteger(string inputPrompt, int maxInput)
+        private static int ReadInputAsInteger(string inputPrompt, int presetInput, int maxInput)
         {
-        LabelReadInput:
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write($"         {inputPrompt} ");
+            Console.CursorVisible = false;
+            
+            int chosenInput = presetInput;
+            bool inputAdjusted = false;
 
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            string userInput = Console.ReadLine() ?? string.Empty;
-
-            if (int.TryParse(userInput, out int output) == false)
+            while (inputAdjusted == false)
             {
-                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
-                Console.WriteLine(new string(' ', Console.BufferWidth));
-                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"        │ {inputPrompt} ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{chosenInput}");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("        └─────");
 
-                goto LabelReadInput;
+                char pressedKey = (char)Console.ReadKey(true).Key;
+                
+                switch (pressedKey)
+                {
+                    case (char)ConsoleKey.UpArrow:
+                        if (chosenInput + 1 <= maxInput)
+                        {
+                            chosenInput = chosenInput + 1;
+                        }
+                        Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 2);
+                        Console.WriteLine(new string(' ', Console.BufferWidth));
+                        Console.WriteLine(new string(' ', Console.BufferWidth));
+                        Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 2);
+                        break;
+
+                    case (char)ConsoleKey.DownArrow:
+                        if (chosenInput - 1 >= 5)
+                        {
+                            chosenInput = chosenInput - 1;
+                        }
+                        Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 2);
+                        Console.WriteLine(new string(' ', Console.BufferWidth));
+                        Console.WriteLine(new string(' ', Console.BufferWidth));
+                        Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 2);
+                        break;
+
+                    case (char)ConsoleKey.Enter:
+                        inputAdjusted = true;
+                        Console.WriteLine();
+                        break;
+
+                    case (char)ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 2);
+                        Console.WriteLine(new string(' ', Console.BufferWidth));
+                        Console.WriteLine(new string(' ', Console.BufferWidth));
+                        Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 2);
+                        break;
+                }
             }
 
-            if (output > maxInput || output < 5)
-            {
-                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
-                Console.WriteLine(new string(' ', Console.BufferWidth));
-                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+            Console.CursorVisible = true;
 
-                goto LabelReadInput;
-            }
-
-            return output;
+            return chosenInput;
         }
     }
 }
